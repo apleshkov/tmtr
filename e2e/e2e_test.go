0d9e8ca331869ac6ef6f505bcb51c364a01f83a9
apleshkov <andrew.pleshkov@gmail.com>
2024-11-12 18:07:36 +0300

    new end-to-end tests

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -27,8 +27,14 @@ func TestText(t *testing.T) {
 		t,
 		generate(
 			`{{. | html}}`,
-			gen.ModeText,
-			newStringData(`O'Reilly: How are <i>you</i>?`),
+			gen.GeneratorOptions{
+				Mode:     gen.ModeText,
+				DataType: "string",
+				FnName:   "render",
+			},
+			[]file{
+				newBasicMainFile("render", fmt.Sprintf("%q", `O'Reilly: How are <i>you</i>?`)),
+			},
 		),
 		`O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?`,
 	)
@@ -42,22 +48,111 @@ func TestHTML(t *testing.T) {
 		t,
 		generate(
 			`<html><head><title>{{.}}</title></head><body><p>{{.}}</p></body></html>`,
-			gen.ModeHTML,
-			newStringData(`O'Reilly: How are <i>you</i>?`),
+			gen.GeneratorOptions{
+				Mode:     gen.ModeHTML,
+				DataType: "string",
+				FnName:   "render",
+			},
+			[]file{
+				newBasicMainFile("render", fmt.Sprintf("%q", `O'Reilly: How are <i>you</i>?`)),
+			},
 		),
 		`<html><head><title>O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?</title></head><body><p>O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?</p></body></html>`,
 	)
 }
 
-type data struct {
-	ty, val string
+func TestMaybeValue(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	util.TestEq(
+		t,
+		generate(
+			`{{maybe foo .}}`,
+			gen.GeneratorOptions{
+				Mode:     gen.ModeHTML,
+				DataType: "string",
+				Funcs:    []string{"foo"},
+				FnName:   "render",
+			},
+			[]file{
+				newBasicMainFile("render", fmt.Sprintf("%q", `bar`)),
+				{
+					name:    "foo.go",
+					content: "package main\nfunc foo(s string) (string, error) { return s, nil }",
+				},
+			},
+		),
+		"bar",
+	)
 }
 
-func newStringData(s string) data {
-	return data{
-		ty:  "string",
-		val: fmt.Sprintf("%q", s),
+func TestMaybeError(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
 	}
+	util.TestEq(
+		t,
+		generate(
+			`{{maybe foo .}}`,
+			gen.GeneratorOptions{
+				Mode:     gen.ModeHTML,
+				DataType: "string",
+				Funcs:    []string{"foo"},
+				FnName:   "render",
+			},
+			[]file{
+				newBasicMainFile("render", fmt.Sprintf("%q", `bar`)),
+				{
+					name:    "foo.go",
+					content: "package main\nimport \"errors\"\nfunc foo(s string) (string, error) { return \"\", errors.New(\"invalid arg: \"+s) }",
+				},
+			},
+		),
+		"invalid arg: bar\n",
+	)
+}
+
+func TestCallMethod(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	util.TestEq(
+		t,
+		generate(
+			`{{call .foo}}`,
+			gen.GeneratorOptions{
+				Mode:     gen.ModeHTML,
+				DataType: "data",
+				FnName:   "render",
+			},
+			[]file{
+				newBasicMainFile("render", "data{}"),
+				{
+					name:    "data.go",
+					content: "package main\ntype data struct {}\nfunc (data) foo() string { return \"bar\" }",
+				},
+			},
+		),
+		"bar",
+	)
+}
+
+type file struct {
+	name, content string
+}
+
+func newMainFile(content string) file {
+	return file{
+		name:    "main.go",
+		content: content,
+	}
+}
+
+func newBasicMainFile(fn, data string) file {
+	return newMainFile(
+		fmt.Sprintf("package main\nimport \"os\"\nfunc main() { %s(os.Stdout, %s, os.Stdout) }\n", fn, data),
+	)
 }
 
 func runCommand(cmd *exec.Cmd) {
@@ -85,7 +180,7 @@ func mustx[T any](v T, err error) T {
 	return v
 }
 
-func generate(tmpl string, mode gen.Mode, data data) string {
+func generate(tmpl string, opts gen.GeneratorOptions, files []file) string {
 	var testName string
 	if pc, _, _, ok := runtime.Caller(1); ok {
 		n := runtime.FuncForPC(pc).Name()
@@ -102,17 +197,48 @@ func generate(tmpl string, mode gen.Mode, data data) string {
 	defer func() {
 		must(os.RemoveAll(tmpdir))
 	}()
-	inFile := mustx(createInputFile(tmpdir, tmpl, mode))
+	var modeStr string
+	switch opts.Mode {
+	case gen.ModeHTML:
+		modeStr = "html"
+	case gen.ModeText:
+		modeStr = "text"
+	default:
+		panic(fmt.Sprintf("invalid mode: %v", opts.Mode))
+	}
+	inFile := mustx(createInputFile(tmpdir, tmpl, modeStr))
 	outFile := path.Join(tmpdir, "render.go")
-	runCommand(
-		exec.Command("./tmtr", "-pkg", "main", "-fn", "render", "-type", data.ty, "-in", inFile, "-out", outFile),
-	)
-	main := fmt.Sprintf("package main\nimport \"os\"\nfunc main() { render(os.Stdout, %s, os.Stdout) }\n", data.val)
-	must(os.WriteFile(path.Join(tmpdir, "main.go"), []byte(main), os.ModePerm))
+	runCommandFunc(func() *exec.Cmd {
+		args := []string{
+			"-pkg", "main",
+			"-mode", modeStr,
+			"-fn", opts.FnName,
+			"-type", opts.DataType,
+			"-in", inFile,
+			"-out", outFile,
+		}
+		for _, v := range opts.Tmpls {
+			if len(v.DataType) > 0 {
+				args = append(args, "-tpl", v.Name+":"+v.DataType)
+			} else {
+				args = append(args, "-tpl", v.Name)
+			}
+		}
+		for _, v := range opts.Imports {
+			args = append(args, "-import", v)
+		}
+		for _, v := range opts.Funcs {
+			args = append(args, "-tplfn", v)
+		}
+		cmd := exec.Command("./tmtr", args...)
+		return cmd
+	})
+	for _, f := range files {
+		must(os.WriteFile(path.Join(tmpdir, f.name), []byte(f.content), os.ModePerm))
+	}
 	ver := strings.Replace(runtime.Version(), "go", "go ", 1)
 	funcsdep := fmt.Sprintf("%s v0.0.0-unpublished", gen.FuncsPkgPath)
 	gomod := fmt.Sprintf("module %s\n%s\nrequire %s\nreplace %s => ../../funcs", testName, ver, funcsdep, funcsdep)
-	println(gomod)
 	must(os.WriteFile(path.Join(tmpdir, "go.mod"), []byte(gomod), os.ModePerm))
 	var buf strings.Builder
 	runCommandFunc(func() *exec.Cmd {
@@ -123,17 +249,8 @@ func generate(tmpl string, mode gen.Mode, data data) string {
 	return buf.String()
 }
 
-func createInputFile(dir, src string, mode gen.Mode) (string, error) {
-	name := "input."
-	switch mode {
-	case gen.ModeText:
-		name += "txt"
-	case gen.ModeHTML:
-		name += "html"
-	default:
-		return "", fmt.Errorf("invalid mode: %d", mode)
-	}
-	p := path.Join(dir, name)
+func createInputFile(dir, src string, ext string) (string, error) {
+	p := path.Join(dir, "input."+ext)
 	err := os.WriteFile(p, []byte(src), os.ModePerm)
 	return p, err
 }
